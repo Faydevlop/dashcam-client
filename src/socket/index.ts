b139e1d0fbb687595b72d70ba63078e0c252e0a1
fayis <fayfayis74@gmail.com>
2025-07-22 15:34:49 +0530

    added vidoe call function

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -2,12 +2,12 @@ import { io } from "socket.io-client";
 
 export const socket = io("https://8b5127919bc7.ngrok-free.app", {
   path: "/socket.io",
-  transports: ["websocket", "polling"], // Fallback to polling if WebSocket fails
+  transports: ["websocket", "polling"],
   secure: true,
   reconnection: true,
   reconnectionAttempts: 10,
   reconnectionDelay: 1000,
   extraHeaders: {
-    "ngrok-skip-browser-warning": "true", // Bypass ngrok's browser warning
+    "ngrok-skip-browser-warning": "true",
   },
 });
